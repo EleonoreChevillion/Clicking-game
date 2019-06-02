48d5970fdb361fb0823be4b89b5f0df087ffadbb
Eleonore Chevillion <el.chevillion@gmail.com>
2019-06-02 17:03:14 -0400

    commit changes

---
+++ build/precache-manifest.5b8de58e5470b6b502fe78014382a061.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b4a10fe5dd714d95b0f057dbcb915d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27c70e701c2df447c38c",
+    "url": "/static/css/main.7978c976.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "27c70e701c2df447c38c",
+    "url": "/static/js/main.7ae9b9c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
